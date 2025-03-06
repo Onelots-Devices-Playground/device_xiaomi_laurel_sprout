b1bc362038aeefb5fcec540638932f73b996be0e
Onelots <onelots@onelots.fr>
2025-03-06 19:23:41 +0100

    laurel_sprout: inherit DEVICE_IS_UDFPS flag
    Signed-off-by: Onelots <onelots@onelots.fr>

--- device.mk
+++ device.mk
@@ -9,6 +9,7 @@ $(call inherit-product, device/xiaomi/sm6125-common/common.mk)
 
 # EvolutionX flags
 TARGET_USES_PICO_GAPPS := true
+TARGET_HAS_UDFPS := true
 
 DEVICE_PATH := device/xiaomi/laurel_sprout
 
