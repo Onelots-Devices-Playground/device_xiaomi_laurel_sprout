631c430d90fcfc560237301f6829456430418c46
Onelots <onelots@onelots.fr>
2025-03-10 12:31:48 +0100

    laurel_sprout: Welcome to EvolutionX
    Signed-off-by: Onelots <onelots@onelots.fr>

--- device.mk
+++ device.mk
@@ -57,7 +57,8 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-lineage \
+    $(DEVICE_PATH)/overlay-evolution
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(DEVICE_PATH)/overlay-lineage/packages/apps/Aperture
